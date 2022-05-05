855fd090a7176ea0a04c3c90dbc2348ee2c185fb
stephenv13 <stephenvogelmeier@gmail.com>
2022-05-04 21:22:10 -0400

    added navbar functionality

--- workout_app/urls.py
+++ workout_app/urls.py
@@ -4,6 +4,8 @@ from workout_app import views
 
 urlpatterns = [
     path('',views.home,name='home'),
+    path('login/',views.account_login,name='login'),
+    path('workouts/',views.workouts,name='workouts'),
 
 
 ]
--- workout_app/views.py
+++ workout_app/views.py
@@ -2,5 +2,14 @@ from django.shortcuts import render,get_object_or_404,redirect
 
 # Create your views here.
 
+# This returns the homepage HTML template
 def home(request):
     return render(request,'home.html',{})
+
+# This returns the account login HTML template
+def account_login(request):
+    return render(request,'login.html',{})
+
+# This returns the workouts HTML template
+def workouts(request):
+    return render(request,'workouts.html',{})
